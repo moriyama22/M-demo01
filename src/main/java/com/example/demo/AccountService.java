b71cb441722fbf46b33d71dec53fe5cd815a219f
moriyama22 <amatukatosiki@gmail.com>
2025-06-15 02:07:20 +0900

    Fix invalid import in AccountService.java

--- src/main/java/com/example/demo/AccountService.java
+++ src/main/java/com/example/demo/AccountService.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.entity.Account;
 import com.example.demo.repository.AccountRepository;
-import com.example.*;
+
 
 @Service
 public class AccountService {
